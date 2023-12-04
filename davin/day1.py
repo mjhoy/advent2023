b29aa674ce6be897b7a2039e91abcbd6d8bd558e
Davin Hill <davin.hill@outlook.com>
2023-12-03 22:18:03 -0500

    reorganize

--- davin/day1.py
+++ davin/day1.py
@@ -4,32 +4,20 @@ import os
 import numpy as np
 import pandas as pd
 
+from utils import *
+
 
 #############################################################
 # Problem 1
 #############################################################
 #%%
 
-def first_integer(string, reverse = False):
-    for i in range(len(string)):
-        if reverse:
-            char = string[-(i+1)]
-        else:
-            char = string[i]
-        try:
-            output = int(char)
-            break
-        except:
-            continue
-    return output
-
-df = pd.read_csv('input.txt', sep = '\t', header = None)
+df = pd.read_csv('Files/input_day1.txt', sep = '\t', header = None)
 df['firstdigit'] = df.loc[:,0].apply(first_integer, reverse = False)
 df['lastdigit'] = df.loc[:,0].apply(first_integer, reverse = True)
 df['code'] = (df['firstdigit'].astype('str') + df['lastdigit'].astype('str')).astype('int')
 answer1 = df['code'].sum()
 print('Answer 1: %s' % str(answer1))
-
 
 #############################################################
 # Problem 2
@@ -49,35 +37,6 @@ digit_list = [
 ]
 digit_list += map(str, np.arange(10).tolist())
 digit_mapping = np.arange(1,10).tolist() + np.arange(10).tolist() # map digit_list to integer
-
-def first_number_singledigit(string, search_number, reverse = False):
-    # returns the index of when search_number first appears. Returns None if not found.
-    search_len = len(search_number)
-    
-    if reverse:
-        for i in range(len(string)-search_len, -1, -1):
-            substr = string[i:i+search_len]
-            if substr == search_number:
-                return i
-    else:
-        for i in range(len(string)+1-search_len):
-            substr = string[i:i+search_len]
-            if substr == search_number:
-                return i
-    return None
-
-def first_number(string, digit_list, digit_mapping, reverse = False):
-    # wrapper that applies first_number_singledigit over a list of digits
-    digit_index = []
-    for digit in digit_list:
-        digit_index.append(first_number_singledigit(string, digit, reverse = reverse))
-
-    if reverse:
-        optimal_idx = digit_index.index(max([d for d in digit_index if d is not None])) # get index of digit that appears first
-    else:
-        optimal_idx = digit_index.index(min([d for d in digit_index if d is not None])) # get index of digit that appears first
-    return digit_mapping[optimal_idx] # convert index to integer from digit_mapping
-
 
 df['firstnum'] = df.loc[:,0].apply(first_number, digit_list = digit_list, digit_mapping = digit_mapping, reverse = False)
 df['lastnum'] = df.loc[:,0].apply(first_number, digit_list = digit_list, digit_mapping = digit_mapping, reverse = True)
---
+++ davin/utils.py
@@ -0,0 +1,50 @@
+
+
+
+#############################################################
+# Day 1
+#############################################################
+def first_integer(string, reverse = False):
+    for i in range(len(string)):
+        if reverse:
+            char = string[-(i+1)]
+        else:
+            char = string[i]
+        try:
+            output = int(char)
+            break
+        except:
+            continue
+    return output
+
+def first_number_singledigit(string, search_number, reverse = False):
+    # returns the index of when search_number first appears. Returns None if not found.
+    search_len = len(search_number)
+    
+    if reverse:
+        for i in range(len(string)-search_len, -1, -1):
+            substr = string[i:i+search_len]
+            if substr == search_number:
+                return i
+    else:
+        for i in range(len(string)+1-search_len):
+            substr = string[i:i+search_len]
+            if substr == search_number:
+                return i
+    return None
+
+def first_number(string, digit_list, digit_mapping, reverse = False):
+    # wrapper that applies first_number_singledigit over a list of digits
+    digit_index = []
+    for digit in digit_list:
+        digit_index.append(first_number_singledigit(string, digit, reverse = reverse))
+
+    if reverse:
+        optimal_idx = digit_index.index(max([d for d in digit_index if d is not None])) # get index of digit that appears first
+    else:
+        optimal_idx = digit_index.index(min([d for d in digit_index if d is not None])) # get index of digit that appears first
+    return digit_mapping[optimal_idx] # convert index to integer from digit_mapping
+
+#############################################################
+# Day 1
+#############################################################
